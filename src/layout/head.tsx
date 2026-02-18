476b7e8b6850c3db3193ee03566d8cd5edd874c1
liliangxiong2846 <liliangxiong2846@gmail.com>
2026-02-18 17:04:34 +0800

    调整：设置页面默认缩放为80%

--- src/layout/head.tsx
+++ src/layout/head.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export default function Head() {
 	return (
 		<head>
-			<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+			<meta name='viewport' content='width=device-width, initial-scale=0.8, maximum-scale=5.0, user-scalable=yes' />
 			<link rel='manifest' href='/manifest.json' />
 
 			<link rel='icon' href='/favicon.png' />
